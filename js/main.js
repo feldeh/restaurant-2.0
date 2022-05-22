508c35d9aa6e9d845fce4196e7571fe62354ebd6
fel <felicien.dehertogh@protonmail.com>
2022-05-22 10:13:27 +0200

    Beautify all pages

--- js/main.js
+++ js/main.js
@@ -48,10 +48,10 @@ function initMapBer() {
     });
 }
 
-function loadmaps(){
-        initMapBru();
-        initMapBro();
-        initMapBer();  
+function loadmaps() {
+    initMapBru();
+    initMapBro();
+    initMapBer();
 }
 
 window.loadmaps();
