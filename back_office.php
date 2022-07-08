92bc27f324b11ef637866154a15c252e0c4922ba
fel <felicien.dehertogh@protonmail.com>
2022-07-08 12:08:53 +0200

    Add modal to admin edit

--- back_office.php
+++ back_office.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin</title>
+    <title>Admin Dashboard</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -14,8 +14,6 @@
 </head>
 
 <style>
-
-
     table {
         width: 100%;
         min-width: 670px;
@@ -29,22 +27,25 @@
         word-wrap: break-word;
     }
 
-    tr > :first-child  {
+    tr> :first-child {
         width: 2.1rem;
     }
 
-    tr > :last-child {
+    tr> :last-child {
         width: 3.8rem;
     }
 
-    tr > :nth-child(2), tr > :nth-child(3), tr > :nth-child(4), tr > :nth-child(5), tr > :nth-child(6) {
+    tr> :nth-child(2),
+    tr> :nth-child(3),
+    tr> :nth-child(4),
+    tr> :nth-child(5),
+    tr> :nth-child(6) {
         width: 5.8rem;
     }
-    
-thead {
-    position: sticky;
-}
 
+    thead {
+        position: sticky;
+    }
 </style>
 
 <body>
@@ -53,20 +54,12 @@ thead {
         include __DIR__ . '/php/config.php';
         include __DIR__ . '../../helpers.php';
 
-
         $stmt = $pdo->query('SELECT * FROM contacts');
-
-        // while ($row = $stmt->fetch(PDO::FETCH_OBJ)) {
-        //     echo $row->contact_mail . '<br>';
-        // }   
-
-
         ?>
 
         <nav id="nav" class="navbar navbar-default navbar-expand-lg">
             <div class="container">
                 <a class="navbar-brand fs-2" href="./index.php">Mt Foodji
-                    <!-- <img src="" alt=""> -->
                 </a>
                 <button class="navbar-toggler custom-toggler flex-end" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -83,7 +76,7 @@ thead {
         </nav>
 
         <div class="p-2">
-            <h2 class="text-center mb-4">Contacts</h2>
+            <h2 class="text-center mb-4">Admin Dashboard</h2>
             <table class="table table-striped table-hover table-bordered ">
                 <thead>
                     <tr class="sticky-top">
@@ -109,30 +102,112 @@ thead {
                             <td><?php echo $row['restaurant']; ?></td>
                             <td><?php echo $row['subject']; ?></td>
                             <td><?php echo $row['message']; ?></td>
-
-
-
-
                             <td>
-                                <a href="./php/update.php?id=<?php echo $row['contact_id']; ?>" class="m-2">
-                                    <i class="fa fa-edit fa-2x"></i>
-                                </a>
-                                <div>
-                                    <i class="fa fa-trash fa-2x red-icon" data-bs-toggle="modal" data-bs-target="#exampleModal<?php echo $row['contact_id']; ?>">
 
+                                <!-- Edit button -->
+
+                                <!-- <a href="./php/edit.php?id=<?php echo $row['contact_id']; ?>" class="m-2">
+                                    <i class="fa fa-edit fa-2x"></i>
+                                </a> -->
+
+                                <div>
+
+
+                                    <i class="fa fa-edit btn btn-primary" data-bs-toggle="modal" data-bs-target="#edit-modal<?php echo $row['contact_id']; ?>">
                                     </i>
                                 </div>
 
-                                <div class="modal fade" id="exampleModal<?php echo $row['contact_id']; ?>" role="dialog">
+                                <div class="modal fade" id="edit-modal<?php echo $row['contact_id']; ?>" role="dialog">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <div class="modal-body">
+
+
+
+                                                <form class="rounded-3 p-2 needs-validation" action="./php/edit.php?id=<?php echo $row['contact_id']; ?>" method="post" novalidate>
+                                                    <h2 class="pb-2 border-bottom border-3 border-dark">Edit contact</h2>
+                                                    <div class="row mb-2 pt-4">
+
+
+                                                        <div class="col mb-3">
+                                                            <label for="name" class="form-label">Name</label>
+                                                            <input type="text" name="name" class="form-control" id="name" aria-describedby="nameHelp" required>
+                                                            <div class="invalid-feedback">
+                                                                Please provide your name.
+                                                            </div>
+                                                        </div>
+
+
+                                                        <div class="col mb-3">
+                                                            <label for="mail" class="form-label">Email</label>
+                                                            <input type="mail" name="mail" class="form-control" id="mail" aria-describedby="mailHelp" required>
+                                                            <div class="invalid-feedback">
+                                                                Please provide your email.
+                                                            </div>
+                                                        </div>
+
+                                                    </div>
+
+
+                                                    <div class="mb-3">
+                                                        <label for="restaurant" class="form-label">Restaurant</label>
+                                                        <select class="form-select" name="restaurant" id="restaurant" aria-label="Default select example" required>
+                                                            <option selected disabled value="">Select restaurant</option>
+                                                            <option value="brooklyn">Brooklyn</option>
+                                                            <option value="brussels">Brussels</option>
+                                                            <option value="berlin">Berlin</option>
+                                                        </select>
+                                                    </div>
+
+
+                                                    <div class="mb-3">
+                                                        <label for="subject" class="form-label">Subject</label>
+                                                        <select class="form-select" name="subject" id="subject" aria-label="Default select example" required>
+                                                            <option selected disabled value="">Select subject</option>
+                                                            <option value="reservation">Reservation</option>
+                                                            <option value="general">General information</option>
+                                                            <option value="compliment">Compliment</option>
+                                                            <option value="complaint">Complaint</option>
+                                                        </select>
+                                                    </div>
+
+
+                                                    <div class="form-floating mb-3">
+                                                        <textarea class="form-control" name="message" id="exampleFormControlTextarea1" placeholder="Message" style="height: 15em;"></textarea required>
+                                                        <label for="floatingTextarea2">Message</label>
+                                                    </div>
+
+                                                </form>
+
+                                            </div>
+                                            <div class="modal-footer justify-content-center">
+
+                                                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancel</button>
+
+                                                <button type="submit" name="submit" class="btn btn-primary">Save</button>
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <!-- Delete button -->
+
+                                <div>
+                                    <i class="fa fa-trash btn btn-danger" data-bs-toggle="modal" data-bs-target="#delete-modal<?php echo $row['contact_id']; ?>">
+                                    </i>
+                                </div>
+
+                                <div class="modal fade" id="delete-modal<?php echo $row['contact_id']; ?>" role="dialog">
                                     <div class="modal-dialog">
                                         <div class="modal-content">
                                             <div class="modal-body">
                                                 <h5>Do you want to delete this contact?</h5>
                                             </div>
                                             <div class="modal-footer justify-content-center">
-                                                <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Cancel</button>
+                                                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancel</button>
                                                 <a href="./php/delete.php?id=<?php echo $row['contact_id']; ?>">
-                                                    <button class="btn btn-danger" type="button">Confirm</button>
+                                                    <button class="btn btn-primary" type="button">Confirm</button>
                                                 </a>
                                             </div>
                                         </div>
